f5ee8cac2637d3c93b280edcbd199287a24663a9
Andreas Riemer <andreas.riemer@gmail.com>
2012-07-14 10:42:55 +0200

    Adds timeout option for json provider

--- lib/hoth.rb
+++ lib/hoth.rb
@@ -2,6 +2,7 @@ require "hoth/version"
 require 'singleton'
 
 require 'active_support/inflector'
+require 'active_support/core_ext/module/attribute_accessors'
 
 require 'hoth/transport/hoth_transport'
 require 'hoth/transport/json_transport'
@@ -20,4 +21,7 @@ require 'hoth/extension/core/exception'
 require 'hoth/exceptions'
 
 module Hoth
+  module Config
+    mattr_accessor :timeout
+  end
 end
--- lib/hoth/transport/json_transport.rb
+++ lib/hoth/transport/json_transport.rb
@@ -14,12 +14,30 @@ module Hoth
         Errno::ECONNREFUSED,
         SocketError
       ]
-      
+
+      ##
+      # The usual Net::HTTP.post_form does not respect the read_timeout setting,
+      # so this method just do the same and sets the timeout value
+      #
+      def post_form(url, params)
+        request = Net::HTTP::Post.new(url.path)
+        request.form_data = params
+        req.basic_auth url.user, url.password if url.user
+        http = Net::HTTP.new(url.host, url.port)
+        if timeout = Hoth::Config.timeout
+          http.read_timeout = timeout
+        end
+        http.start do |started|
+          started.request(request)
+        end
+      end
+
       def call_remote_with(*args)
         uri = URI.parse(self.endpoint.to_url)
         params = { 'name' => self.name.to_s, 'params' => args.to_json }
         Hoth::Logger.info "Connecting to '#{uri}' with #{params.inspect}."
-        response = Net::HTTP.post_form(uri, params)
+
+        response = post_form(uri, params)
         case response
         when Net::HTTPSuccess
           begin
@@ -43,7 +61,7 @@ module Hoth
         when Net::HTTPRedirection, Net::HTTPClientError, Net::HTTPInformation, Net::HTTPUnknownResponse
           #TODO Handle redirects(3xx) and http errors(4xx), http information(1xx), unknown responses(xxx)
           raise NotImplementedError, "HTTP code: #{response.code}, message: #{response.message}"
-        end       
+        end
       rescue *CONNECTION_ERRORS => e
         Hoth::Logger.error "Connecting to '#{uri}' with #{params.inspect} failed with #{e.class}: #{([e] + e.backtrace) * "\n"}"
         raise ConnectionError.wrap(e)
